1d9698e474846c07fdad08be7d2cdca45e80a9d0
tostacam <edgar-tostado@hotmail.com>
2026-04-03 12:40:33 -0700

    copy ctrs, destructor

--- include/cpp-core/memory/unique_ptr.h
+++ include/cpp-core/memory/unique_ptr.h
@@ -9,6 +9,17 @@ template <typename T>
 class unique_ptr{
 public:
   unique_ptr() : ptr(nullptr) {}
+  
+  explicit unique_ptr(T* ptr) : ptr(ptr) {}
+
+  unique_ptr(const unique_ptr&) = delete;
+
+  unique_ptr& operator=(const unique_ptr&) = delete;
+
+  ~unique_ptr(){
+    delete ptr; /* allocation: new T(...) */
+  } 
+
 private:
   T* ptr;
 };
