4edfbbb36595f733868e51d904adcef1de08075f
tostacam <edgar-tostado@hotmail.com>
2026-04-03 19:12:18 -0700

    initial structure

---
+++ include/cpp-core/memory/shared_ptr.h
@@ -0,0 +1,19 @@
+// std::shared_ptr<T>
+// - holds a raw pointer
+// - uses a control block (ref count + deleter)
+// - deletes the object when ref count reaches zero
+// - can be copied (ref count increases), can be moved (transfers ownership)
+// - allows shared ownership (multiple owners)
+// - prevents double delete via reference counting
+
+template <typename T>
+class shared_ptr{
+private:
+  T* ptr_;
+  control_block* ctrl_;
+};
+
+class control_block{
+public:
+  size_t ref_count;
+}
