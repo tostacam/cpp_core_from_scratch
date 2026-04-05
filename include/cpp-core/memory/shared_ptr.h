a191547d62dc531ff5e1411c9758074a072f5950
tostacam <edgar-tostado@hotmail.com>
2026-04-04 17:17:38 -0700

    fix copy and move ctr mechanics

--- include/cpp-core/memory/shared_ptr.h
+++ include/cpp-core/memory/shared_ptr.h
@@ -6,8 +6,10 @@
 // - allows shared ownership (multiple owners)
 // - prevents double delete via reference counting
 
+#pragma once
 #include <cstddef>  /* for size_t */
 
+#include <iostream>
 class control_block{
 public:
   size_t ref_count;
@@ -26,16 +28,51 @@ public:
   }
 
   shared_ptr(const shared_ptr& other){
+    std::cout << "from copy ctr\n";
     ptr_ = other.ptr_;
     ctrl_ = other.ctrl_;
-    ctrl_->ref_count++;
+    if(ctrl_)
+      ctrl_->ref_count++;
   }
 
   shared_ptr& operator=(const shared_ptr& other){
+    if(this != &other){ /* in case of self copy */
+      /* remove current pointer */
+      if(ctrl_ && --(ctrl_->ref_count) == 0){
+        delete ptr_;
+        delete ctrl_;
+      }
+
+      ptr_ = other.ptr_;
+      ctrl_ = other.ctrl_;
+      if(ctrl_)
+        ctrl_->ref_count++;
+    }
+    return *this;
+  }
+
+  shared_ptr(shared_ptr&& other){
+    std::cout << "from move ctr\n";
     ptr_ = other.ptr_;
     ctrl_ = other.ctrl_;
-    ctrl_->ref_count++;
-    return *this; 
+    other.ptr_ = nullptr;
+    other.ctrl_ = nullptr;
+  }
+
+  shared_ptr& operator=(shared_ptr&& other){
+    if(this != &other){ /* in case of self move */
+      /* release current */
+      if(ctrl_ && --(ctrl_->ref_count) == 0){
+        delete ptr_;
+        delete ctrl_;
+      }
+
+      ptr_ = other.ptr_;
+      ctrl_ = other.ctrl_;
+      other.ptr_ = nullptr;
+      other.ctrl_ = nullptr;
+    }
+    return *this;
   }
 
   ~shared_ptr(){
