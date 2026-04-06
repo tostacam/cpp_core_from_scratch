6d9a6849ea7191fe8b8f52e1a0fb79c950ac6bc4
tostacam <edgar-tostado@hotmail.com>
2026-04-06 15:13:14 -0700

    swap init

---
+++ include/cpp-core/utilities/swap.h
@@ -0,0 +1,12 @@
+// std::swap()
+// - use move operations to swap data
+
+#pragma once
+#include "move.h"
+
+template <typename T>
+void swap(T& a, T& b){
+  T temp = move(a);
+  a = move(b);
+  b = move(temp);
+}
