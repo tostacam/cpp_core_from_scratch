489db6da79cc94f03d88d16c771eb2e829a08d69
tostacam <edgar-tostado@hotmail.com>
2026-04-06 14:51:23 -0700

    prevent rvalue->lvalue cast

--- include/cpp-core/utilities/forward.h
+++ include/cpp-core/utilities/forward.h
@@ -5,11 +5,23 @@
 #include "remove_reference.h"
 
 template <typename T>
+struct is_lvalue_reference {
+  static constexpr bool value = false;
+};
+
+template <typename T>
+struct is_lvalue_reference<T&> {
+  static constexpr bool value = false;
+};
+
+template <typename T>
 T&& forward(remove_reference_t<T>& t){
   return static_cast<T&&>(t);  
 }
 
 template <typename T>
 T&& forward(remove_reference_t<T>&& t){
+  /* stops forwarding of rvalue as lvalue */
+  static_assert(!is_lvalue_reference<T>::value, "wrong forward");
   return static_cast<T&&>(t);
 }
