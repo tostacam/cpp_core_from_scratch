855a6eac95a4550d002cb8f38b59d97da3e93216
tostacam <edgar-tostado@hotmail.com>
2026-04-09 10:52:05 -0700

    custom comp sort

--- include/cpp-core/algorithms/sort.h
+++ include/cpp-core/algorithms/sort.h
@@ -9,12 +9,11 @@ It partition_qs(It first, It last){
   auto pivot = *(last - 1);
   It i = first;
   
-  for(It j = first; j != last - 1; ++j){
+  for(It j = first; j != last - 1; ++j)
     if(*j < pivot){
       swap(*i, *j);
       ++i;
     }
-  }
 
   swap(*i, *(last - 1));
   return i;
@@ -29,4 +28,30 @@ void sort(It first, It last){
 
   sort(first, p);
   sort(p + 1, last);  
+}
+
+template <typename It, typename Comp>
+It partition_qs(It first, It last, Comp comp){
+  auto pivot = *(last - 1);
+  It i = first;
+
+  for(It j = first; j != last; ++j)
+    if(comp(*j, pivot)){
+      swap(*i, *j);
+      ++i;
+    }
+
+  swap(*i, *(last - 1));
+  return i;
+}
+
+template <typename It, typename Comp>
+void sort(It first, It last, Comp comp){
+  if(last - first <= 1)
+    return;
+
+  It p = partition_qs(first, last, comp);
+
+  sort(first, p, comp);
+  sort(p + 1, last, comp);
 }
