d162139eff64f75d8c68e374f9c1c58885267d84
tostacam <edgar-tostado@hotmail.com>
2026-04-08 20:50:50 -0700

    sort init

---
+++ include/cpp-core/algorithms/sort.h
@@ -0,0 +1,32 @@
+// std::sort()
+
+#pragma once
+
+#include "../utilities/swap.h"
+
+template <typename It>
+It partition_qs(It first, It last){
+  auto pivot = *(last - 1);
+  It i = first;
+  
+  for(It j = first; j != last - 1; ++j){
+    if(*j < pivot){
+      swap(*i, *j);
+      ++i;
+    }
+  }
+
+  swap(*i, *(last - 1));
+  return i;
+}
+
+template <typename It>
+void sort(It first, It last){
+  if(last - first <= 1)
+    return;
+
+  It p = partition_qs(first, last);
+
+  sort(first, p);
+  sort(p + 1, last);  
+}
