31868c5365cc3c6a94eb3d15554be3b7b33f5b5e
tostacam <edgar-tostado@hotmail.com>
2026-04-09 16:39:38 -0700

    make heap init

---
+++ include/cpp-core/algorithms/make_heap.h
@@ -0,0 +1,38 @@
+// std::make_heap()
+
+#pragma once
+
+#include "../utilities/swap.h"
+
+template <typename It>
+void make_heap(It first, It last){
+  if(last - first <= 1)
+    return;
+
+  for(auto i = first + ((last - first) / 2 - 1); ; --i){
+    It root = i;
+
+    while(true){
+      auto left = first + (2 * (root - first) + 1);
+      auto right = left + 1;
+
+      if(left >= last)
+        break;
+
+      It largest = left;
+
+      if(right < last && *left < *right)
+        largest = right;
+
+      if(*root < *largest){
+        swap(*root, *largest);
+        root = largest;
+      }
+      else
+        break;
+    }
+
+    if(i == first)
+      break;
+  }
+}
