c07765da4ba5cda4e7013707799bfd7aaeba325f
tostacam <edgar-tostado@hotmail.com>
2026-04-09 11:32:39 -0700

    median for partition

--- include/cpp-core/algorithms/sort.h
+++ include/cpp-core/algorithms/sort.h
@@ -8,7 +8,7 @@ template <typename It>
 It partition_qs(It first, It last){
   auto pivot = *(last - 1);
   It i = first;
-  
+
   for(It j = first; j != last - 1; ++j)
     if(*j < pivot){
       swap(*i, *j);
@@ -24,6 +24,15 @@ void sort(It first, It last){
   if(last - first <= 1)
     return;
 
+  It mid = first + (last - first) / 2;
+  if(*mid < *first)
+    swap(*mid, *first);
+  if(*(last - 1) < *first)
+    swap(*(last - 1), *first);
+  if(*(last - 1) < *mid)
+    swap(*(last - 1), *mid);
+  swap(*mid, *(last -1));
+
   It p = partition_qs(first, last);
 
   sort(first, p);
@@ -35,7 +44,7 @@ It partition_qs(It first, It last, Comp comp){
   auto pivot = *(last - 1);
   It i = first;
 
-  for(It j = first; j != last; ++j)
+  for(It j = first; j != last - 1; ++j)
     if(comp(*j, pivot)){
       swap(*i, *j);
       ++i;
@@ -49,6 +58,15 @@ template <typename It, typename Comp>
 void sort(It first, It last, Comp comp){
   if(last - first <= 1)
     return;
+
+  It mid = first + (last - first) / 2;
+  if(comp(*mid, *first))
+    swap(*mid, *first);
+  if(comp(*(last - 1), *first))
+    swap(*(last - 1), *first);
+  if(comp(*(last - 1), *mid))
+    swap(*(last - 1), *mid);
+  swap(*mid, *(last -1));
 
   It p = partition_qs(first, last, comp);
 
