ba4db9bc301d04843d3b9864375262cad629e0ac
tostacam <edgar-tostado@hotmail.com>
2026-04-09 15:57:29 -0700

    push heap init

---
+++ include/cpp-core/algorithms/push_heap.h
@@ -0,0 +1,21 @@
+// std::push_heap()
+
+#pragma once
+
+#include "../utilities/swap.h"
+
+template <typename It>
+void push_heap(It first, It last){
+  auto child = last - 1;
+  
+  while(child > first){
+    auto parent = first + (child - 1 - first) / 2;
+    
+    if(*parent < *child){
+      swap(*parent, *child);
+      child = parent;
+    }
+    else 
+      break;
+  }
+}
