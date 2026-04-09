4b0bb46d166ae18bc170ca7a7cd8a9aef310d991
tostacam <edgar-tostado@hotmail.com>
2026-04-09 11:51:47 -0700

    insertion sort for small ranges

--- include/cpp-core/algorithms/sort.h
+++ include/cpp-core/algorithms/sort.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "../utilities/swap.h"
+#define RANGE_SIZE  16
 
 template <typename It>
 It partition_qs(It first, It last){
@@ -19,8 +20,64 @@ It partition_qs(It first, It last){
   return i;
 }
 
+template <typename It, typename Comp>
+It partition_qs(It first, It last, Comp comp){
+  auto pivot = *(last - 1);
+  It i = first;
+
+  for(It j = first; j != last - 1; ++j)
+    if(comp(*j, pivot)){
+      swap(*i, *j);
+      ++i;
+    }
+
+  swap(*i, *(last - 1));
+  return i;
+}
+
+template <typename It>
+void insertion_sort(It first, It last){
+  if(first == last)
+    return;
+
+  for(It i = first + 1; i != last; ++i){
+    auto value = *i;
+    It j = i;
+
+    while(j != first && value < *(j - 1)){
+      *j = *(j - 1);
+      --j;
+    }
+
+    *j = value;
+  }
+}
+
+template <typename It, typename Comp>
+void insertion_sort(It first, It last, Comp comp){
+  if(first == last)
+    return;
+
+  for(It i = first + 1; i != last; ++i){
+    auto value = *i;
+    It j = i;
+
+    while(j != first && comp(value, *(j - 1))){
+      *j = *(j - 1);
+      --j;
+    }
+
+    *j = value;
+  }
+}
+
 template <typename It>
 void sort(It first, It last){
+  if(last - first < RANGE_SIZE){
+    insertion_sort(first, last);
+    return;
+  }
+
   if(last - first <= 1)
     return;
 
@@ -40,22 +97,12 @@ void sort(It first, It last){
 }
 
 template <typename It, typename Comp>
-It partition_qs(It first, It last, Comp comp){
-  auto pivot = *(last - 1);
-  It i = first;
-
-  for(It j = first; j != last - 1; ++j)
-    if(comp(*j, pivot)){
-      swap(*i, *j);
-      ++i;
-    }
-
-  swap(*i, *(last - 1));
-  return i;
-}
-
-template <typename It, typename Comp>
 void sort(It first, It last, Comp comp){
+  if(last - first < RANGE_SIZE){
+    insertion_sort(first, last, comp);
+    return;
+  }
+  
   if(last - first <= 1)
     return;
 
