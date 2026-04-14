80fa236c8270f6f212bddec3da13a061d8a81fbc
tostacam <edgar-tostado@hotmail.com>
2026-04-14 13:52:16 -0700

    event storage

--- include/cpp-core/systems/event_bus.h
+++ include/cpp-core/systems/event_bus.h
@@ -1,8 +1,30 @@
+#pragma once
+#include "../containers/vector.h"
+
+namespace cpp_core{
+
+template <typename Event>
+struct event_storage{
+  static cpp_core::vector<void(*)(const Event&)> listeners;
+};
+
+template <typename Event>
+cpp_core::vector<void(*)(const Event&)> event_storage<Event>::listeners;
+
 class event_bus {
 public:
-  template <typename Event, typename Func>
-  void subscribe(Func&& f);
+  template <typename Event>
+  void subscribe(void(*f)(const Event&)){
+    event_storage<Event>::listeners.push_back(f); 
+  }
 
   template <typename Event>
-  void emit(const Event& e);
+  void emit(const Event& e){
+    auto& listeners = event_storage<Event>::listeners;
+
+    for(auto it = listeners.begin(); it != listeners.end(); ++it)
+      (*it)(e);
+  }
 };
+
+}
