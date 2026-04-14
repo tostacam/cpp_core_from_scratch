3eaf3541d3917ea291617aac7fd13842eaf627e7
tostacam <edgar-tostado@hotmail.com>
2026-04-14 15:36:34 -0700

    std::function on event

--- include/cpp-core/systems/event_bus.h
+++ include/cpp-core/systems/event_bus.h
@@ -1,20 +1,21 @@
 #pragma once
 #include "../containers/vector.h"
+#include "../utilities/function.h"
 
 namespace cpp_core{
 
 template <typename Event>
 struct event_storage{
-  static cpp_core::vector<void(*)(const Event&)> listeners;
+  static cpp_core::vector<cpp_core::function<void(const Event&)>> listeners;
 };
 
 template <typename Event>
-cpp_core::vector<void(*)(const Event&)> event_storage<Event>::listeners;
+cpp_core::vector<cpp_core::function<void(const Event&)>> event_storage<Event>::listeners;
 
 class event_bus {
 public:
   template <typename Event>
-  void subscribe(void(*f)(const Event&)){
+  void subscribe(cpp_core::function<void(const Event&)> f){
     event_storage<Event>::listeners.push_back(f); 
   }
 
--- include/cpp-core/utilities/function.h
+++ include/cpp-core/utilities/function.h
@@ -1,10 +1,47 @@
 // std::function()
 
 #pragma once
+#include "forward.h"
+
+namespace cpp_core{
 
 template <typename>
-class function;
+class function {};
 
 template <typename R, typename... Args>
 class function<R(Args...)> {
+  struct callable_base{
+    virtual R call(Args... args) = 0;
+    virtual ~callable_base() = default;
+  };
+
+  template <typename F>
+  struct callable : callable_base {
+    F f;
+
+    callable(F&& func) : f(cpp_core::forward<F>(func)) {}
+
+    R call(Args... args) override{
+      return f(args...);
+    }
+  };
+
+  callable_base* ptr_ = nullptr;
+
+public:
+
+  template <typename F>
+  function(F&& f){
+    ptr_ = new callable<F>(cpp_core::forward<F>(f));
+  }
+
+  R operator()(Args... args){
+    return ptr_->call(args...);
+  }
+
+  ~function(){
+    delete ptr_;
+  }
 };
+
+}
