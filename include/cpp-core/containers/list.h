11c30dc02ab48c2f44aeffbac5a10246621231a7
tostacam <edgar-tostado@hotmail.com>
2026-04-13 20:01:58 -0700

    push_back, push_front

--- include/cpp-core/containers/list.h
+++ include/cpp-core/containers/list.h
@@ -22,6 +22,35 @@ public:
     delete sentinel_;
   }
 
+  void insert_node(list_node<T>* pos, list_node<T>* node){
+    node->next = pos;
+    node->prev = pos->prev;
+
+    pos->prev->next = node;
+    pos->prev = node;
+  }
+
+  void erase_node(list_node<T>* node){
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+
+    delete node;
+  }
+
+  void push_back(const T& value){
+    list_node<T>* node = new list_node<T>;
+    node->value = value;
+    insert_node(sentinel_, node);
+    ++size_;
+  }
+
+  void push_front(const T& value){
+    list_node<T>* node = new list_node<T>;
+    node->value = value;
+    insert_node(sentinel_->next, node);
+    ++size_;
+  }
+
 private:
   list_node<T>* sentinel_;
   size_t size_;
