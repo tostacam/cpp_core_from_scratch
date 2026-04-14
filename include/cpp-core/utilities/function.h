49d04f430a644c2cf43ba5f2ea4434830b0cfdc2
tostacam <edgar-tostado@hotmail.com>
2026-04-14 15:54:48 -0700

    move ctr and operator

--- include/cpp-core/utilities/function.h
+++ include/cpp-core/utilities/function.h
@@ -29,11 +29,26 @@ class function<R(Args...)> {
   callable_base* ptr_ = nullptr;
 
 public:
-
   template <typename F>
   function(F&& f){
     ptr_ = new callable<F>(cpp_core::forward<F>(f));
   }
+
+  function(function&& other) : ptr_(other.ptr_){
+    other.ptr_ = nullptr;
+  }
+
+  function& operator=(function&& other) {
+    if(this != &other){
+      delete ptr_;
+      ptr_ = other.ptr_;
+      other.ptr_ = nullptr;
+    }
+    return *this;
+  }
+
+  function(const function&) = delete;
+  function& operator=(const function&) = delete;
 
   R operator()(Args... args){
     return ptr_->call(args...);
