93217af0986ff0400fdadd6b6bf97effbf7daa17
tostacam <edgar-tostado@hotmail.com>
2026-04-13 19:23:36 -0700

    ctr, dtr

--- include/cpp-core/containers/list.h
+++ include/cpp-core/containers/list.h
@@ -1,5 +1,7 @@
 // std::list()
 
+#include <cstddef>  /* for size_t */
+
 template <typename T>
 struct list_node{
   T value;
@@ -10,11 +12,17 @@ struct list_node{
 template <typename T>
 class list{
 public:
-  list();
+  list() : size_(0) {
+    sentinel_ = new list_node<T>;
+    sentinel_->prev = sentinel_;
+    sentinel_->next = sentinel_;
+  }
 
-  ~list();
+  ~list(){
+    delete sentinel_;
+  }
 
 private:
-  list_node<T> *sentinel_;
-  size_t size;
+  list_node<T>* sentinel_;
+  size_t size_;
 };
